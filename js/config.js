4c277bc3d0358d164c07ec311b040fb1398175d0
korneevscp <vladimirivanof2024@gmail.com>
2025-09-10 02:50:30 +0200

    add js

---
+++ js/config.js
@@ -0,0 +1,170 @@
+//       _           _    _                     _ 
+//      | |         | |  | |                   | |
+//   ___| | ___  ___| | _| |     ___   __ _  __| |
+//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+//     version 2.16
+
+// URL for the server logo
+// Leave it blank to use text instead
+// (Put the files in the images folder)
+// (You can also use external URL)
+var l_serverImage = "12.png";
+
+// Center logo?
+// 'true' or 'false'
+var l_centerLogo = false;
+
+// Display current map and gamemode?
+// 'true' or 'false'
+var l_displayMapGamemode = false;
+
+// Custom server name configuration
+// Leave it blank for automatic server name
+var l_serverName = "Nyxeria ~ SCP Roleplay";
+
+// Use a video for the background?
+// Otherwise it will use images
+// 'true' or 'false'
+var l_bgVideo = true;
+
+// Default available videos:
+// "aurora.webm"
+// "bluefall.webm"
+// "bluetilefloor.webm"
+// "bluewhitecircles.webm"
+// "bokehlens.webm"
+// "cleanbokeh.webm"
+// "coldnight.webm"
+// "colorfulburst.webm"
+// "dancingstars.webm"
+// "dropsflares.webm"
+// "focusorangebokeh.webm"
+// "glimmering.webm"
+// "glowingrings.webm"
+// "greencircles.webm"
+// "hues.webm"
+// "inthedeep.webm"
+// "modernflourish.webm"
+// "orangeaura.webm"
+// "warminside.webm"
+// "yellowgreen.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format 
+// (Place the videos in the backgrounds/videos folder)
+// (You can also use external URL)
+var l_background = "12.webm";
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// 'true' or 'false'
+var l_bgImageMapBased = false;
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the files in the backgrounds/images folder)
+// (You can also use external URL)
+var l_bgImages = [
+	"touhou01.jpg",
+	"touhou02.jpg",
+	"touhou03.jpg"
+];
+
+// (Image-background only)
+// Random background images order?
+// 'true' or 'false'
+var l_bgImagesRandom = false;
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+var l_bgImageDuration = 5000;
+
+// (Image-background only)
+// Image background fade velocity
+// in milliseconds
+var l_bgImageFadeVelocity = 2000;
+
+// Enable background overlay?
+// 'true' or 'false'
+var l_bgOverlay = true;
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+var l_bgDarkening = 50;
+
+// Play music during the loading screen?
+// 'true' or 'false'
+var l_music = true;
+
+// Display current music name?
+// 'true' or 'false'
+var l_musicDisplay = true;
+
+// Music playlist
+// Add as many youtube ids/urls or ogg files you want
+// (Place the ogg files in the music folder)
+// (You can also use external URL for the ogg files)
+var l_musicPlaylist = [
+	{ogg: "12.ogg", name: "SCP Cinematic Universe Intro - ananomalouswriter YTB"},
+];
+
+// Random music order?
+// 'true' or 'false'
+var l_musicRandom = false;
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+var l_musicVolume = 15;
+
+// Enable custom messages?
+// 'true' or 'false'
+var l_messagesEnabled = true;
+
+// Enter your custom messages below
+var l_messages = [
+	"Bienvenue sur Nyxeria ~ SCP Roleplay",
+	"Bienvenue à la Fondation… là où la vérité dépasse l’imagination.",
+	"La sécurité, le confinement et la protection ne sont pas des choix… mais des obligations.",
+	"Chaque anomalie a son secret… serez-vous celui qui le découvrira ?",
+	"Dans l'ombre, la Fondation veille… et vous aussi.",
+	"Restez vigilant, car dans chaque coin sombre, une anomalie peut se cacher.",
+	"Votre mission est cruciale… la sécurité mondiale en dépend.",
+	"Explorez, découvrez, mais surtout… survivez.",
+	"Chaque jour est une nouvelle opportunité de protéger l'humanité.",
+	"Le savoir est une arme… utilisez-le judicieusement.",
+	"Dans ce monde de mystères, votre rôle est plus important que jamais.",
+];
+
+// Random message order?
+// 'true' or 'false'
+var l_messagesRandom = false;
+
+// Delay between message changes
+// in milliseconds
+var l_messagesDelay = 5000;
+
+// Messages fade time
+// in milliseconds
+var l_messagesFade = 1000;
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// 'true' or 'false'
+var l_showErrorsIngame = true;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};

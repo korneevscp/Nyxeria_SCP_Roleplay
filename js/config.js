153986b2d630d90332b5a8d1d6aeec708b19bc6d
korneevscp <vladimirivanof2024@gmail.com>
2025-09-22 17:22:05 +0200

    fjdsffghdg

--- js/config.js
+++ js/config.js
@@ -55,7 +55,7 @@ var l_bgVideo = false;
 // Just make sure they are using webm VP8 format 
 // (Place the videos in the backgrounds/videos folder)
 // (You can also use external URL)
-var l_background = "12.webm";
+var l_background = "";
 
 // (Image-background only)
 // Enable map-based background image?
